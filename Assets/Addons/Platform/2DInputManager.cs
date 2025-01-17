67ffa1186817b9d588c438ebc65a15a02cc66ac2
enva <1975341859@qq.com>
2025-01-17 21:44:15 +0800

    1

--- Assets/Addons/Platform/2DInputManager.cs
+++ Assets/Addons/Platform/2DInputManager.cs
@@ -21,7 +21,6 @@ public class InputManager : MonoBehaviour
 
     #region 能力选择
     private InputAction _chooseAction;
-    private int selectedIndex = 0;
     #endregion
 
     private void Awake()
@@ -53,16 +52,11 @@ public class InputManager : MonoBehaviour
         #endregion
 
         #region 能力选择
-        if (Input.GetKeyDown(KeyCode.Alpha1)) { selectedIndex = 0; }
-        if (Input.GetKeyDown(KeyCode.Alpha2)) { selectedIndex = 1; }
-        if (Input.GetKeyDown(KeyCode.Alpha3)) { selectedIndex = 2; }
-        if (Input.GetKeyDown(KeyCode.Alpha4)) { selectedIndex = 3; }
-        if (Input.GetKeyDown(KeyCode.Alpha5)) { selectedIndex = 4; }
-        if (Input.GetKeyDown(KeyCode.Alpha6)) { selectedIndex = 5; }
-        if (Input.GetKeyDown(KeyCode.Alpha7)) { selectedIndex = 6; }
-        if (Input.GetKeyDown(KeyCode.Alpha8)) { selectedIndex = 7; }
+        if (Input.GetKeyDown(KeyCode.Alpha1)) { Ability .选中能力= 0; }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) { Ability .选中能力= 1; }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) { Ability .选中能力= 2; }
+        if (Input.GetKeyDown(KeyCode.Alpha4)) { Ability .选中能力= 3; } 
     }
-
     #endregion
 }
 
