641529df3c08f027316cfefc0bc6bd5407d2739a
NekoD <huangqiongtong@gmail.com>
2025-01-18 00:50:04 +0800

    Fixed

--- Assets/Scripts/LevelSettings.cs
+++ Assets/Scripts/LevelSettings.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "LevelSettings", menuName = "LevelSettings", order = 1)]
 public class LevelSettings : ScriptableObject
 {
+    public string 关卡名称;
+    public int 关卡序号;
     public int 总可用能力次数;
     public List<可用能力> 可用能力列表;
 }
