26f8212f16925a6fb448f9248b5fedbca7353e00
NekoD <huangqiongtong@gmail.com>
2025-01-17 23:26:46 +0800

    Bubble

--- Assets/Scripts/AbilityController.cs
+++ Assets/Scripts/AbilityController.cs
@@ -6,11 +6,13 @@ public class AbilityController : ViewController
 {
     public LevelSettings mLevelSettings;
 
-    public AblilityHolder AblilityHolder;
+    public NekoUI NekoUI;
+    public AbilityHolder AblilityHolder;
 
     public int 剩余可用能力次数;
-
     public int 选中能力;
+
+
 
     public void Awake()
     {
@@ -27,16 +29,7 @@ public class AbilityController : ViewController
 
     private void OnGUI()
     {
-        IMGUIHelper.SetDesignResolution(960, 640);
-        string 可用能力 = "";
-        foreach (var item in mLevelSettings.可用能力列表)
-        {
-            int num = (int)item;
-            可用能力 += " " + AblilityBase.能力表[num];
-        }
-        GUILayout.Label("可用能力 " + 可用能力);
-        GUILayout.Label("选中能力 " + AblilityBase.能力表[选中能力]);
-        GUILayout.Label("总可用能力次数:" + mLevelSettings.总可用能力次数);
+        NekoUI.选中能力.text = "选中能力：" + AbilityBase.能力表[选中能力];
     }
     public void Update()
     {
--- Assets/Scripts/UI/UI.Designer.cs
+++ Assets/Scripts/UI/UI.Designer.cs
@@ -1,18 +1,14 @@
-// Generate Id:7c24e73e-d872-4067-9662-aac884d94548
+// Generate Id:55d565f0-f821-453b-b594-6d70cf50b38d
 using UnityEngine;
 
 // 1.请在菜单 编辑器扩展/Namespace Settings 里设置命名空间
 // 2.命名空间更改后，生成代码之后，需要把逻辑代码文件（非 Designer）的命名空间手动更改
 namespace QFramework.Example
 {
-	public partial class Player
+	public partial class UI
 	{
 
-		public BoxCollider2D Feet;
-
-		public BoxCollider2D Body;
-
-		public QFramework.Example.AbilityHolder AblilityHolder;
+		public UnityEngine.UI.Text 选中能力;
 
 	}
 }
