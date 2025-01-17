aeacbd0f3b5ebe182be764705a7f7b7a93b6b124
NekoD <huangqiongtong@gmail.com>
2025-01-17 22:41:44 +0800

    Spike

--- Assets/Scripts/Abilities/BubbleStandable.cs
+++ Assets/Scripts/Abilities/BubbleStandable.cs
@@ -3,14 +3,42 @@ using UnityEngine;
 public class BubbleNormal : MonoBehaviour
 {
     public float 上升速度 = 1f;
+    public Rigidbody2D rb;
 
+
+    private bool bomb = false;
     private void Awake()
     {
         enabled = false;
     }
-    private void Update()
+
+    private void Start()
     {
-        // 每秒上升
-        transform.position += new Vector3(0, 上升速度 * Time.deltaTime, 0);
+        rb = GetComponent<Rigidbody2D>();
+    }
+   
+    private void FixedUpdate()
+    {
+        if(!bomb)
+        {
+            // 每秒上升
+            rb.velocity = new Vector2(0, 上升速度);
+        }
+    }
+
+    public void Bomb()
+    {
+        Debug.Log("破碎");
+        bomb = true;
+        // 播动画
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Spike"))
+        {
+            Bomb();
+        }
     }
 }
--- Assets/Scripts/AbilityController.cs
+++ Assets/Scripts/AbilityController.cs
@@ -17,6 +17,14 @@ public class AbilityController : ViewController
         剩余可用能力次数 = mLevelSettings.总可用能力次数;
     }
 
+    private void Start()
+    {
+        var a = AblilityHolder.BubbleNormal.InstantiateWithParent(transform.Find("Root"))
+                        .LocalPosition(new Vector3(0, 2));
+        a.GetComponent<BubbleNormal>().enabled = true;
+        a.GetComponent<SpriteRenderer>().enabled = true;
+    }
+
     private void OnGUI()
     {
         IMGUIHelper.SetDesignResolution(960, 640);
