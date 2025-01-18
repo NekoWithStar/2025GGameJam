6ae6dfc6b1844c8a994d25a901fc4ff267c0856c
enva <1975341859@qq.com>
2025-01-18 21:01:23 +0800

    esc暂停游戏（待添加选关）

--- Assets/Addons/Platform/2DInputManager.cs
+++ Assets/Addons/Platform/2DInputManager.cs
@@ -6,6 +6,7 @@ public class InputManager : MonoBehaviour
     public static PlayerInput PlayerInput;
     public 移动 Player;
     public AbilityController Ability;
+    public bool IsPaused;
 
     #region 2D平台移动
     public static Vector2 Movement; // 移动方向
@@ -27,7 +28,7 @@ public class InputManager : MonoBehaviour
     {
         Ability = GameObject.FindObjectOfType<AbilityController>();
         PlayerInput = GetComponent<PlayerInput>();
-
+        IsPaused = false;
         #region 2D平台移动
         _moveAction = PlayerInput.actions["Move"];
         _jumpAction = PlayerInput.actions["Jump"];
@@ -46,24 +47,45 @@ public class InputManager : MonoBehaviour
 
         Player.RunIsHeld = _runAction.IsPressed();
         #endregion
-
-        #region 能力选择
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (!IsPaused)
         {
-            Ability.listIndex = 0;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha2))
+            #region 能力选择
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                Ability.listIndex = 0;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                Ability.listIndex = 1;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                Ability.listIndex = 2;
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                Ability.listIndex = 3;
+            }
+            #endregion
+            #region 暂停游戏
+            if (Input.GetKeyUp(KeyCode.Escape))
+            {
+                Time.timeScale = 0;
+                IsPaused = true;
+            }
+            #endregion
+            #region 重新加载场景
+            if (Input.GetKeyUp(KeyCode.R))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+            }
+            #endregion
+        }else if (Input.GetKeyUp(KeyCode.Escape))
         {
-            Ability.listIndex = 1;
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha3)) { Ability .listIndex= 2; }
-        if (Input.GetKeyDown(KeyCode.Alpha4)) { Ability .listIndex= 3; } 
-
-        if(Input.GetKeyUp(KeyCode.R))
-        {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+            Time.timeScale = 1;
+            IsPaused = false;
         }
     }
-    #endregion
+    
 }
 
